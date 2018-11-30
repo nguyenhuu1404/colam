045769f02ffd1e93aabbba833af708584d0443ed
huunv90 <nguyenhuu140490@gmail.com>
2018-11-30 13:52:25 +0700

    huunv

--- public/hls-config.php
+++ public/hls-config.php
@@ -1,4 +1,4 @@
 <?php
 define('BASE_URL', 'http://colam.vn');
 define('BASE_DIR', dirname(__FILE__));
-define('STREAM_DIR', BASE_DIR . '/storage/video');
+define('STREAM_DIR', BASE_DIR . '/videos');
