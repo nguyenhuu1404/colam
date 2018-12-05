68eff000599321f461d91e3d8dd82ef5399c5a38
huunv90 <nguyenhuu140490@gmail.com>
2018-12-06 00:12:34 +0700

    huunv

---
+++ app/Http/Controllers/Frontend/FaqController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FaqController extends Controller
+{
+    public function index(){
+
+    }
+}
