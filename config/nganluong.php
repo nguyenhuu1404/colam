8c998a5189fc22ccec13e1af70a07bd8b7dff6f5
huunv90 <nguyenhuu140490@gmail.com>
2018-12-20 09:36:15 +0700

    huunv

--- config/nganluong.php
+++ config/nganluong.php
@@ -6,26 +6,26 @@ return [
      *
      *
      * */
-    'url_api' => env('URL_API', 'https://sandbox.nganluong.vn:8088/nl30/checkout.api.nganluong.post.php'),
+    'url_api' => env('URL_API', 'https://www.nganluong.vn/checkout.api.nganluong.post.php'),
 
     /*
      * Mã kết nối
      *
      *
      * */
-    'merchant_id' => env('MERCHANT_ID', '36680'),
+    'merchant_id' => env('MERCHANT_ID', '57514'),
 
     /*
      * Mật khẩu kết nối
      *
      *
      * */
-    'merchant_password' => env('MERCHANT_PASSWORD', 'matkhauketnoi'),
+    'merchant_password' => env('MERCHANT_PASSWORD', 'f51b597d7446d94fecf5557df7ba8ffa'),
 
     /*
      * Email tài khoản Ngân Lượng
      *
      *
      * */
-    'receiver_email' => env('RECEIVER_EMAIL', 'demo@nganluong.vn')
+    'receiver_email' => env('RECEIVER_EMAIL', 'lamdaothanh305@gmail.com')
 ];
